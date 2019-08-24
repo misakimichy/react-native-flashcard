774dff5140878c34ce56a44887906fd76ddd2577
Misaki Koonce <misaki.koonce@gmail.com>
2019-08-24 13:26:53 -0700

    Feat: add Card and Deck model.

--- models/Card.js
+++ models/Card.js
@@ -2,20 +2,24 @@ import { guide } from '../utils'
 
 class Card {
   constructor(deckID) {
-    this.deckID = deckID
-    this.id = guide()
+    this.deckID = deckID;
+    this.questions = questions;
+    this.title = title;
+    this.id = guide();
   }
 
   setFromObject = object => {
-    this.deckID = object.deckID
-    this.id = object.id
+    this.deckId = object.deckId;
+    this.id = object.id;
+    this.questions = object.questions;
+    this.title = object.title;
   }
 
   static fromObject = object => {
-    let card = new Card(object.deckID)
-    card.setFromObject(object)
-    return card
+    let card = new Card(object.deckID);
+    card.setFromObject(object);
+    return card;
   }
 }
 
-export default Card
+export default Card;
--- models/Deck.js
+++ models/Deck.js
@@ -2,26 +2,26 @@ import { guide } from '../utils'
 
 class Deck {
   constructor(name) {
-    this.name = name
-    this.id = guide()
-    this.cards = []
+    this.name = name;
+    this.id = guide();
+    this.cards = [];
   }
 
   setFromObject = object => {
-    this.name = object.name
-    this.cards = object.cards
-    this.id = object.id
+    this.name = object.name;
+    this.cards = object.cards;
+    this.id = object.id;
   }
 
   static fromObject = object => {
-    let deck = new Deck(object.name)
-    deck.setFromObject(object)
-    return deck
+    let deck = new Deck(object.name);
+    deck.setFromObject(object);
+    return deck;
   }
 
   addCard = card => {
-    this.cards = this.cards.concat(card)
+    this.cards = this.cards.concat(card);
   }
 }
 
-export default Deck
+export default Deck;
