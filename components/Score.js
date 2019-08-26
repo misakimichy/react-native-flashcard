f8440ca376089a25a20caf98583a6bb2d849052e
Misaki Koonce <misaki.koonce@gmail.com>
2019-08-26 01:00:02 -0700

    Feat: Recreate Score component. Accidently deleted it.

---
+++ components/Score.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { withNavigation } from 'react-navigation';
+import styles from '../style/styles';
+
+const Score = props => {
+    const { correct, incorrect, restart, deck, deckId, navigation } = props;
+
+    return(
+        <View style={styles.center}>
+            <Text style={styles.score}>Correct: {correct}</Text>
+            <Text style={styles.score}>Incorrect: {incorrect}</Text>
+            <Text style={styles.score}>{Math.round((correct/deck.questions.length)*100)}%</Text>
+
+            <TouchableOpacity
+                style={[styles.btn, {backgroundColor: black, marginTop: 25}]}
+                onPress={restart}
+            >
+                <Text style={styles.btnText}>Restart!</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity
+                style={[styles.btn, {backgroundColor: white, marginTop: 25}]}
+                onPress={() => navigation.navigate('Deck', {deckId: deckId, deckName: deck.title})}
+            >
+                <Text style={styles.btnText}>Back to Deck</Text>
+            </TouchableOpacity>
+        </View>
+    )
+}
+
+export default withNavigation(Score);
