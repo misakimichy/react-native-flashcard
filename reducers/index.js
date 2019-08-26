4a49d86baff5c3914855b5647f7056a3b17b0bab
Misaki Koonce <misaki.koonce@gmail.com>
2019-08-25 23:30:31 -0700

    Feat: Organize reducers.

--- reducers/index.js
+++ reducers/index.js
@@ -1,7 +1,30 @@
-import DecksReducer from './decks';
+import { ADD_CARD, ADD_DECK, RECEIVE_DECKS } from '../actions'
 
-export const reducer = (state = initialState(), action) => {
-    let decks = DecksReducer(state.decks, action)
-    return { decks };
-};
-
+export default function decks(state={}, action){
+    switch (action.type) {
+        case ADD_CARD:
+            return {
+                ...state,
+                [action.deckId]: {
+                    title: state[action.deckId].title,
+                    questions: state[action.deckId].questions.concat({
+                        question: action.question,
+                        answer: action.answer
+                    })
+                }
+            }
+        case ADD_DECK: {
+            return {
+                ...state,
+                [action.deckId]: action.deck
+            }
+        }
+        case RECEIVE_DECKS:
+            return {
+                ...state,
+                ...action.decks
+            }
+        default:
+            return state
+    }
+}
